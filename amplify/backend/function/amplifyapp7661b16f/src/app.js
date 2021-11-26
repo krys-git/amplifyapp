1429b1c0d574870348ec5a85a38897e938295cda
krys-git <hlmtri92@gmail.com>
2021-11-26 14:30:42 +0700

    20211126 midday push

---
+++ amplify/backend/function/amplifyapp7661b16f/src/app.js
@@ -0,0 +1,215 @@
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+
+const AWS = require('aws-sdk')
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+var bodyParser = require('body-parser')
+var express = require('express')
+
+AWS.config.update({ region: process.env.TABLE_REGION });
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+let tableName = "dynamof9484251";
+if(process.env.ENV && process.env.ENV !== "NONE") {
+  tableName = tableName + '-' + process.env.ENV;
+}
+
+const userIdPresent = false; // TODO: update in case is required to use that definition
+const partitionKeyName = "eventid";
+const partitionKeyType = "S";
+const sortKeyName = "timestamp";
+const sortKeyType = "N";
+const hasSortKey = sortKeyName !== "";
+const path = "/events";
+const UNAUTH = 'UNAUTH';
+const hashKeyPath = '/:' + partitionKeyName;
+const sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+// convert url string param to expected Type
+const convertUrlType = (param, type) => {
+  switch(type) {
+    case "N":
+      return Number.parseInt(param);
+    default:
+      return param;
+  }
+}
+
+// Create the DynamoDB Client with the region you want
+const region = 'ap-northeast-1';
+const dynamoDbClient = createDynamoDbClient(region);
+
+// Create the input for scan call
+const scanInput = createScanInput();
+
+ // Call DynamoDB's scan API
+executeScan(dynamoDbClient, scanInput).then(() => {
+    console.info('Scan API call has been executed.')
+  }
+);
+
+function createDynamoDbClient(regionName) {
+  // Set the region
+  AWS.config.update({region: regionName});
+  // Use the following config instead when using DynamoDB Local
+  // AWS.config.update({region: 'localhost', endpoint: 'http://localhost:8000', accessKeyId: 'access_key_id', secretAccessKey: 'secret_access_key'});
+  return new AWS.DynamoDB();
+}
+
+function createScanInput() {
+  return {
+    "TableName": "dynamo2e7a9e53",
+    "ConsistentRead": false
+  }
+}
+
+async function executeScan(dynamoDbClient, scanInput) {
+  // Call DynamoDB's scan API
+  try {
+    const scanOutput = await dynamoDbClient.scan(scanInput).promise();
+    console.info('Scan successful.');
+    return scanOutput;
+    // Handle scanOutput
+  } catch (err) {
+    handleScanError(err);
+  }
+}
+
+function handleScanError(err) {
+  if (!err) {
+    console.error('Encountered error object was empty');
+    return;
+  }
+  if (!err.code) {
+    console.error(`An exception occurred, investigate and configure retry strategy. Error: ${JSON.stringify(err)}`);
+    return;
+  }
+  // here are no API specific errors to handle for Scan, common DynamoDB API errors are handled below
+  handleCommonErrors(err);
+}
+
+function handleCommonErrors(err) {
+  switch (err.code) {
+    case 'InternalServerError':
+      console.error(`Internal Server Error, generally safe to retry with exponential back-off. Error: ${err.message}`);
+      return;
+    case 'ProvisionedThroughputExceededException':
+      console.error(`Request rate is too high. If you're using a custom retry strategy make sure to retry with exponential back-off. `
+        + `Otherwise consider reducing frequency of requests or increasing provisioned capacity for your table or secondary index. Error: ${err.message}`);
+      return;
+    case 'ResourceNotFoundException':
+      console.error(`One of the tables was not found, verify table exists before retrying. Error: ${err.message}`);
+      return;
+    case 'ServiceUnavailable':
+      console.error(`Had trouble reaching DynamoDB. generally safe to retry with exponential back-off. Error: ${err.message}`);
+      return;
+    case 'ThrottlingException':
+      console.error(`Request denied due to throttling, generally safe to retry with exponential back-off. Error: ${err.message}`);
+      return;
+    case 'UnrecognizedClientException':
+      console.error(`The request signature is incorrect most likely due to an invalid AWS access key ID or secret key, fix before retrying. `
+        + `Error: ${err.message}`);
+      return;
+    case 'ValidationException':
+      console.error(`The input fails to satisfy the constraints specified by DynamoDB, `
+        + `fix input before retrying. Error: ${err.message}`);
+      return;
+    case 'RequestLimitExceeded':
+      console.error(`Throughput exceeds the current throughput limit for your account, `
+        + `increase account level throughput before retrying. Error: ${err.message}`);
+      return;
+    default:
+      console.error(`An exception occurred, investigate and configure retry strategy. Error: ${err.message}`);
+      return;
+  }
+}
+
+/************************************
+ * HTTP Get method for list objects *
+ ************************************/
+
+app.get(path, async function(req, res) {
+  try {
+    const data = await executeScan(dynamoDbClient, scanInput);
+    res.statusCode = 200;
+    res.json(data);
+  } catch (err) {
+    handleScanError(err);
+    res.statusCode = 500;
+    res.json({error: err});
+  }
+});
+
+/*****************************************
+ * HTTP Get method for get single object *
+ *****************************************/
+
+app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
+  var params = {};
+  if (userIdPresent && req.apiGateway) {
+    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
+  } else {
+    params[partitionKeyName] = req.params[partitionKeyName];
+    try {
+      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
+    } catch(err) {
+      res.statusCode = 500;
+      res.json({error: 'Wrong column type ' + err});
+    }
+  }
+  if (hasSortKey) {
+    try {
+      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
+    } catch(err) {
+      res.statusCode = 500;
+      res.json({error: 'Wrong column type ' + err});
+    }
+  }
+
+  let getItemParams = {
+    TableName: tableName,
+    Key: params
+  }
+
+  dynamodb.get(getItemParams,(err, data) => {
+    if(err) {
+      res.statusCode = 500;
+      res.json({error: 'Could not load items: ' + err.message});
+    } else {
+      if (data.Item) {
+        res.json(data.Item);
+      } else {
+        res.json(data) ;
+      }
+    }
+  });
+});
+
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
